c5725d28fe4075aa71d7b47c4768df3b78cdc0a5
loziuk <loziukvika10@gmail.com>
2023-04-03 22:06:57 +0300

    css fix

--- History.php
+++ History.php
@@ -4,7 +4,7 @@ require_once 'vendor/autoload.php';
 $loader = new \Twig\Loader\FilesystemLoader('templates');
 $twig = new \Twig\Environment($loader);
 
-$style = "css/History.css";
+$style = "";
 $content = 'History.html.twig';
 $title = 'History';
 echo $twig->render("Base.html.twig", [
---
+++ script.js
@@ -0,0 +1,6 @@
+function aside_hover_on(){
+    document.getElementById('aside').style.display= "flex";
+}
+function aside_hover_down(){
+    document.getElementById('aside').style.display= "none";
+}
